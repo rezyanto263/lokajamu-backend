afa3195883162538ca6041b7eeda958de15e382b
rezyanto263 <rezyanto263@gmail.com>
2024-12-10 00:19:48 +0800

    Recipes API Fix

--- src/models/tipModel.js
+++ src/models/tipModel.js
@@ -2,11 +2,11 @@ const db = require('../config/database');
 
 const Tip = {
   addBatch: (tips) => {
-    const sql = 'INSERT INTO Tips (recipesId, description) VALUES ?';
+    const sql = 'INSERT INTO tips (recipesId, description) VALUES ?';
     return db.query(sql, [tips]);
   },
   deleteAll: (recipeId) => {
-    const sql = 'DELETE FROM Tips WHERE recipesId = ?';
+    const sql = 'DELETE FROM tips WHERE recipesId = ?';
     return db.query(sql, [recipeId]);
   }
 };
