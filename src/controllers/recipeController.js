21be4d66ce84312d104f0a9430602c448540b855
rezyanto263 <rezyanto263@gmail.com>
2024-12-10 00:12:31 +0800

    Recipes API Fix

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -145,7 +145,7 @@ const editRecipe = async (req, res) => {
 
     if (tips) {
       await Tip.deleteAll(recipeId);
-      await Tip.addBatch(tips.map((s) => [recipeId, s.description]));
+      await Tip.addBatch(tips.map((tip) => [recipeId, tip]));
     }
 
     return res.json({
